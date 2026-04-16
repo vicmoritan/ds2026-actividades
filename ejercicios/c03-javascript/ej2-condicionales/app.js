68e9f1db86c27f74ea4ca26453605fda77f0b8a6
vicmoritan <vicmoritan@gmail.com>
2026-04-16 20:26:06 -0300

    eliminación de breaks

--- ejercicios/c03-javascript/ej2-condicionales/app.js
+++ ejercicios/c03-javascript/ej2-condicionales/app.js
@@ -17,13 +17,13 @@ console.log(clasificarNota(9)) //Promocionado
 //Función para determinar qué dia de la semana es según el número
 function diaDeLaSemana(numero){
     switch (numero){
-        case 1: return "Lunes"; break;
-        case 2: return "Martes"; break;
-        case 3: return "Miércoles"; break;
-        case 4: return "Jueves"; break;
-        case 5: return "Viernes"; break;
-        case 6: return "Sábado (fin de semana)"; break;
-        case 7: return "Domingo (fin de semana)"; break;
+        case 1: return "Lunes";
+        case 2: return "Martes";
+        case 3: return "Miércoles";
+        case 4: return "Jueves";
+        case 5: return "Viernes";
+        case 6: return "Sábado (fin de semana)";
+        case 7: return "Domingo (fin de semana)";
         default: return "Día inválido";
     }
 }  
