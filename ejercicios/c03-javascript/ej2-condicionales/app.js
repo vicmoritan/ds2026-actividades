1e3c01633a0e28490441a2c7ee4494a301635c82
vicmoritan <vicmoritan@gmail.com>
2026-04-16 14:08:56 -0300

    Ejercicio 2: Condicionales

---
+++ ejercicios/c03-javascript/ej2-condicionales/app.js
@@ -0,0 +1,34 @@
+//Función para determinar si un alumno promocionó, aprobó o desaprobó
+function clasificarNota(nota){
+    if (nota < 4){
+        return "Desaprobado";
+    } else if (nota >=4 && nota <= 7){
+        return "Aprobado";
+    }   else {
+        return "Promocionado";
+    }
+}
+
+//Datos de prueba para clasificarNota
+console.log(clasificarNota(3)) //Desaprobado
+console.log(clasificarNota(5)) //Aprobado
+console.log(clasificarNota(9)) //Promocionado
+
+//Función para determinar qué dia de la semana es según el número
+function diaDeLaSemana(numero){
+    switch (numero){
+        case 1: return "Lunes"; break;
+        case 2: return "Martes"; break;
+        case 3: return "Miércoles"; break;
+        case 4: return "Jueves"; break;
+        case 5: return "Viernes"; break;
+        case 6: return "Sábado (fin de semana)"; break;
+        case 7: return "Domingo (fin de semana)"; break;
+        default: return "Día inválido";
+    }
+}  
+
+//Datos de prueba para diaDeLaSemana
+console.log(diaDeLaSemana(2)) //Martes
+console.log(diaDeLaSemana(6)) //Sábado (fin de semana)
+console.log(diaDeLaSemana(10)) //Día inválido
