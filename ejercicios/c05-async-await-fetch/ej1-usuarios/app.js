57f278ee888dbae6e90e47bee1fe20d1763e789d
vicmoritan <vicmoritan@gmail.com>
2026-04-30 14:28:10 -0300

    Ejercicio 1: Fetch básico tipado

---
+++ ejercicios/c05-async-await-fetch/ej1-usuarios/app.js
@@ -0,0 +1,27 @@
+"use strict";
+//Función para obtener lista de usuarios desde API
+async function obtenerUsuarios() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        if (!response.ok) {
+            throw new Error(`HTTP ${response.status}`);
+        }
+        const usuarios = await response.json();
+        return usuarios;
+    }
+    catch (error) {
+        console.error('Error al obtener usuarios: ', error);
+        return [];
+    }
+}
+//Función para mostrar los nombres y emails de usuarios obtenidos
+async function mostrarUsuarios() {
+    const listaUsuarios = await obtenerUsuarios();
+    listaUsuarios.forEach(usuario => {
+        console.log(`Nombre: ${usuario.name}`);
+        console.log(`Email: ${usuario.email}`);
+        console.log(`------------------------`);
+    });
+}
+//Llamada a la función para mostrar
+mostrarUsuarios();
---
+++ ejercicios/c05-async-await-fetch/ej1-usuarios/app.ts
@@ -0,0 +1,39 @@
+//Estructura del usuario
+interface Usuario {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+}
+
+//Función para obtener lista de usuarios desde API
+async function obtenerUsuarios(): Promise<Usuario[]> {
+    try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    if (!response.ok) {
+        throw new Error (`HTTP ${response.status}`);
+    }
+
+    const usuarios: Usuario[] = await response.json();
+    return usuarios;
+    }
+    
+    catch (error) {
+        console.error('Error al obtener usuarios: ', error);
+        return [];
+    }
+}
+
+//Función para mostrar los nombres y emails de usuarios obtenidos
+    async function mostrarUsuarios() {
+    const listaUsuarios = await obtenerUsuarios();
+    listaUsuarios.forEach(usuario => {
+        console.log(`Nombre: ${usuario.name}`);
+        console.log(`Email: ${usuario.email}`);
+        console.log(`------------------------`);
+    });
+}
+
+//Llamada a la función para mostrar
+mostrarUsuarios();
